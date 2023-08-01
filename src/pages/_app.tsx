3c8f4da1ffbe24e73f721d2123972aa7f3fbd0a6
Andrei Ce <andrei90@gmail.com>
2023-08-01 16:55:50 -0300

    setup: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+
+// outside App component to load it only once (it would load again if we would access a new page)
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { globalCss } from "@celoco-ui/react"
+
+// @celoco-ui/react uses Stitches - css framework
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+})
